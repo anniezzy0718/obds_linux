880cd70c433164fcb8354ddb4ff8f501b05fd9d3
newc6434 <newc6434@obds.imm.ox.ac.uk>
2025-10-02 11:11:45 +0100

    day4 rnaseq commadn

---
+++ rnaseq_commands.sh
@@ -0,0 +1,7 @@
+load_mamba
+cd /project/newc6434/linux/2_rnaseq/
+fastqc *fastqc.gz /project/newc6434/linux/2_rnaseq/3_analysis/1_fastqc/
+cd /3_analysis/1_fastqc/
+multiqc /project/newc6434/linux/2_rnaseq/3_analysis/reports
+
+
